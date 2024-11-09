77b36c731bce62c79344f2e0c20f3f114ed89ca5
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2024-11-09 02:47:35 -0500

    Creating posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path('post/new/', views.post_new, name='post_new'),
+    path("post/<int:pk>/", views.DetailView.as_view(), name="post_detail"),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path("drafts/", views.DraftView.as_view(), name="post_draft_list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -18,3 +18,37 @@ class DraftView(generic.ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+    """def get_queryset(self):
+        #Excludes any posts that aren't published yet.
+        return Post.objects.filter(published_date__lte=timezone.now())"""
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.created_date = timezone.now()
+            post.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'action': 'Edit', 'form': form})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.created_date = timezone.now()
+            post.save()
+            return redirect('blog:post_draft_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'action': 'New', 'form': form})
