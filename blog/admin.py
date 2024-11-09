b74ec796872f5738f95469c04e6c79dc75576297
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2024-11-08 19:04:14 -0500

    Starting to use models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment
+
+# About Comment
+class CommentInline(admin.StackedInline):
+    model = Comment
+    fields = ["approved_comment", "text", "created_date"]
+    extra = 3
+
+# About Post
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Date information:", {
+            "fields":
+                ["created_date",
+                 "published_date"]
+        }),
+        ("Post information:", {
+            "fields":
+                ["title",
+                 "text",
+                 "active"]
+        }),
+    ]
+    list_display = ["title", "published_date", "active"]
+    list_filter = ["published_date", "created_date"]
+    search_fields = ["title", "text"]
+    inlines = [CommentInline]
+
+admin.site.register(Post, PostAdmin)
