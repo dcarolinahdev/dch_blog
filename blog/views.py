df94cdc5b09c8cae6c27ded8e7da12056d65dcfd
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2024-11-08 22:33:49 -0500

    Generic List Views: index and drafts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from django.views import generic
 
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
-def index(request):
-    return HttpResponse("Testing app's view.")
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now(), active=True).order_by('-published_date')
+
+class DraftView(generic.ListView):
+    template_name = 'blog/drafts.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
